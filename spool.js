9cbdcf44e73c5783f4a3f2cbbae5be7bb13473e2
Andrey Ignatyev <soulseeker-nf@yandex.ru>
2017-05-16 13:31:01 +1000

    removed config node, added configurable status text

--- spool.js
+++ spool.js
@@ -1,36 +1,41 @@
 module.exports = function(RED) 
 {
 	"use strict";
+	
     var reconnect = RED.settings.sqliteReconnectTime || 20000;
     var sqlite3 = require('sqlite3');
 	var util = require('util');
 	
-	var table = "spool3";
+	var table = "spool4";
 	
-	//
-	// Opening, checking and closing the db connection, based on the existing SQLite node
-	//
-	function SpoolNodeDB(config) 
+	function SpoolNode(config)
 	{
-		
         RED.nodes.createNode(this, config);
-
-        this.dbname = config.db;
+		
+		this.dbname = config.dbname;
+		this.connStatusText = config.connStatusText;
+		this.disconnStatusText = config.disconnStatusText;
+		
         var node = this;
 		
-        node.doConnect = function() 
+		var connectionStatus = false;
+		var tableIsEmpty = true;
+		
+		node.doConnect = function() 
 		{
             node.db = new sqlite3.Database(node.dbname);
+			
             node.db.on('open', function() 
 			{
                 if (node.tick) { clearTimeout(node.tick); }
-                node.log("opened " + node.dbname + " ok");
+                node.log("Opened " + node.dbname + " successfully.");
 				
-				node.db.run("CREATE TABLE IF NOT EXISTS " + table + " (msg TEXT)");
+				node.db.run("CREATE TABLE IF NOT EXISTS " + table + " (id INTEGER PRIMARY KEY ASC, msg TEXT)");
             });
+			
             node.db.on('error', function(err) 
 			{
-                node.error("failed to open " + node.dbname, err);
+                node.error("Failed to open " + node.dbname, err + ".");
                 node.tick = setTimeout(function() { node.doConnect(); }, reconnect);
             });
         }
@@ -43,40 +48,24 @@ module.exports = function(RED)
 			node.db.run("DROP TABLE  " + table);
         });
 		
-    }
-    RED.nodes.registerType('spooldb', SpoolNodeDB);
-
-	//
-	// actual operations with the db
-	// 
-	function SpoolNode(config)
-	{
-        RED.nodes.createNode(this, config);
-		
-		this.mydb = config.mydb;
-        this.mydbConfig = RED.nodes.getNode(this.mydb);
-		
-		var connectionStatus = false;
-		var tableIsEmpty = true;
-		
-		if(this.mydbConfig)
+		if(node.dbname != "")
 		{
+		
+			node.doConnect();
 			
-			this.mydbConfig.doConnect();
-			var node = this;
+			console.log(config);
 			
 			node.on('input', function(msg) 
-			{
-				
+			{				
 				if(msg.hasOwnProperty('status'))
 				{					
-					if(msg.status.text.includes("status.connected")) 
+					if(msg.status.text.includes(node.connStatusText)) 
 					{ 
 						connectionStatus = true;
 
-						node.mydbConfig.db.serialize(function()
+						node.db.serialize(function()
 						{
-							node.mydbConfig.db.each("SELECT * FROM " + table, function(err, row)
+							node.db.each("SELECT * FROM " + table + " ORDER BY id ASC", function(err, row)
 							{
 								if(err)
 								{
@@ -84,40 +73,37 @@ module.exports = function(RED)
 								}
 								else
 								{
-									node.mydbConfig.db.serialize(function()
-									{
-										var parsedRow = JSON.parse(row.msg);
-										
-										node.send(parsedRow);
-										console.log(parsedRow._msgid + " was successfully passed on to the downstream node.");
+									var parsedRow = JSON.parse(row.msg);
 									
-										node.mydbConfig.db.run("DELETE FROM " + table + " WHERE msg LIKE (?)", "%" + parsedRow._msgid + "%", function(err) 
-										{
-											if(err) throw err;
-											
-											console.log("Report: " + this.changes + " - " + util.inspect(this, { showHidden: false, depth: null }));
+									node.send(parsedRow);
+									console.log(parsedRow._msgid + " was successfully passed on to the downstream node.");
+								
+									node.db.run("DELETE FROM " + table + " WHERE id = ?", row.id, function(err) 
+									{
+										if(err) throw err;
+										
+										console.log("Report: " + this.changes + " - " + util.inspect(this, { showHidden: false, depth: null }));
 
-											if(this.changes == 1) console.log(parsedRow._msgid + " was successfully removed from the database.");
-											else console.log("Could not remove " + parsedRow._msgid + " from the database.");
+										if(this.changes == 1) console.log(parsedRow._msgid + " was successfully removed from the database.");
+										else console.log("Could not remove " + parsedRow._msgid + " from the database.");
 
-										});									
-									});									
+									});
 								}								
 							});							
 						});						
 					}
-					else if(msg.status.text.includes("status.disconnected")) { connectionStatus = false; }
+					else if(msg.status.text.includes(node.disconnStatusText)) { connectionStatus = false; }
 				}
 				
 				if(msg.hasOwnProperty('topic'))
 				{					
 					if(connectionStatus == false)
 					{						
-						node.mydbConfig.db.serialize(function()
+						node.db.serialize(function()
 						{
 							var stringifiedMsg = JSON.stringify(msg);
 						
-							node.mydbConfig.db.run("INSERT INTO " + table + " VALUES (?)", stringifiedMsg, function(err) 
+							node.db.run("INSERT INTO " + table + " VALUES (NULL, ?)", stringifiedMsg, function(err) 
 							{
 								if(err) throw err;
 								
@@ -133,16 +119,14 @@ module.exports = function(RED)
 					{
 						node.send(msg);
 						console.log(msg._msgid + " was successfully passed on to the downstream node.");
-					}
-					
-				}
-					
+					}					
+				}				
 			});
 			
 		}
 		else
 		{
-			this.error("Sqlite database not configured");
+			this.error("An sqlite database's path is not configured correctly.");
 		}
 			
     }
